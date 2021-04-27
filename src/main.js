8b70e47920aa83777adcfdc9969b7718d8d52bda
nguyennhan <nguyennhan981024@gmail.com>
2021-04-27 14:05:14 +0700

    validate Form

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import store from '@/store';
 
 Vue.config.productionTip = false;
 
+import { ValidationProvider, ValidationObserver } from 'vee-validate';
+import rules from './validate'
+
+rules();
+
+Vue.component('ValidationProvider', ValidationProvider);
+Vue.component('ValidationObserver', ValidationObserver);
+
 new Vue({
   store,
   render: h => h(App)
